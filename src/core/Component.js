c1f067e9385ccb7eda44027385c656f81e6c2b30
subin1224 <salllena33@gmail.com>
2021-08-23 17:00:44 +0900

    [1.0.0] 웹 컴포넌트 작업

---
+++ src/core/Component.js
@@ -0,0 +1,41 @@
+export default class Component {
+    $target;
+    $props;
+    $state;
+
+    constructor ($target, $props) {
+        this.$target = $target;
+        this.$props = $props;
+        this.setup();
+        this.setEvent();
+        this.render();
+    }
+
+    setup () {}
+    mounted () {}
+    template () { return '';}
+    render () {
+        this.$target.innerHTML = this.template();
+        this.mounted();
+    }
+    setEvent () {}
+    setState (newState) {
+        this.$state = { ...this.$state, ...newState };
+        this.render();
+        //localStorage.setItem('todoList', JSON.stringify(this.$state));
+    };
+
+    addEvent (eventType, selector, callback) {
+        const children = [ ...this.$target.querySelectorAll(selector) ];
+        const isTarget = (target) => children.includes(target) || target.closest(selector);
+       
+        this.$target.addEventListener(eventType, event => {
+            if(!isTarget(event.target)) return false;
+            callback(event);
+        });
+    }
+
+    removeEvent (eventType, callback) {
+        this.$target.removeEventListener(eventType, callback);
+    }
+}
